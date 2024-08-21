19ea654068c4759612489605e2bb810e80338842
Bibash45 <bibashcdry46@gmail.com>
2024-08-21 19:01:08 +0545

    react first commit

---
+++ react/demo-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import First from './First';
+import Second from './Second';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React js</h2>
+      <First />
+      <Second />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/demo-app/src/Second.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Second = () => {
+  return (
+    <>
+    <h1>This is the second components</h1>
+    </>
+  )
+}
+
+export default Second
